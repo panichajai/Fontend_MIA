1f72d874fab231a927e8c80f74972c8131a47f30
panichajai <panichaboomjail262@gmail.com>
2024-10-19 21:41:10 +0700

    [ADD] Tab ,List All Pages

--- MIA/mia-client/src/config.js
+++ MIA/mia-client/src/config.js
@@ -1,2 +1,4 @@
-const API_BASE_URL = 'https://api-mia-five.vercel.app/api/';
+// const API_BASE_URL = 'https://api-mia-five.vercel.app/api/';
+const API_BASE_URL = 'http://localhost:3005/api/';
+
 export default API_BASE_URL;
